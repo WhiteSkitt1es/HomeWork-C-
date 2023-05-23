bb1e44a18ddfc2336c82ea51a41c94dcf76ca26b
Vado <vado@MBP-Paul.Dlink>
2023-05-23 19:29:51 +0300

    Task 64 completed

--- Task_64/Program.cs
+++ Task_64/Program.cs
@@ -2,3 +2,14 @@
 // Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.Write($"Введите число: ");
+
+int b = int.Parse(Console.ReadLine()!);
+
+string Numbers (int a, int b)
+{
+    if (a <= b) return Numbers(a + 1, b) + $"{a} ";
+    else return string.Empty; 
+}
+
+Console.WriteLine($" N = {b} -> {Numbers(1,b)}");
