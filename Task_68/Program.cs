28bbe4fd94f38fcd47d2862d256ba514610ebd93
Vado <vado@MBP-Paul.Dlink>
2023-05-23 20:03:56 +0300

    Task 68 completed

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -2,3 +2,28 @@
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write($"Введите число M: ");
+
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write($"Введите число N: ");
+
+int n = int.Parse(Console.ReadLine()!);
+int Numbers (int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return Numbers(m - 1, 1);
+    }
+    else
+    {
+        return Numbers(m - 1, Numbers(m, n - 1));
+    }
+}
+
+Console.WriteLine($"M = {m}; N = {n} -> {Numbers(m,n)}");
