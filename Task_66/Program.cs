4f954e1cf973984b35a5f8e126e7b1c7af1fe426
Vado <vado@MBP-Paul.Dlink>
2023-05-23 19:55:05 +0300

    Task 66 completed

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -2,3 +2,17 @@
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+Console.Write($"Введите число M: ");
+
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write($"Введите число N: ");
+
+int n = int.Parse(Console.ReadLine()!);
+int Numbers (int m, int n)
+{
+    if (m == n) return m;
+    else return n + Numbers(m, n - 1);
+}
+
+Console.WriteLine($"M = {m}; N = {n} -> {Numbers(m,n)}");
